d685baab2cdbecff3685b81117ff234a23b80b6c
Rohit Venugopal <rohitven@cs.utexas.edu>
2016-12-03 13:42:14 -0600

    Billboard always facing camera

--- src/main.cc
+++ src/main.cc
@@ -84,7 +84,12 @@ in vec4 position;
 
 void main()
 {
-	gl_Position = projection * view * position;
+	vec4 center = vec4(0,0,0,1);
+	center = projection * view * center;
+	vec4 cam_up = vec4(view[0][1], view[1][1], view[2][1], view[3][1]);
+	vec4 cam_r = vec4(view[0][0], view[1][0], view[2][0], view[3][0]);
+	vec4 p = center + cam_r * position.x * 100 + cam_up * position.y * 100;
+	gl_Position = projection * view * p;
 }
 )zzz";
 
@@ -257,6 +262,9 @@ int main(int argc, char* argv[])
 
 	// FIXME: Create the RenderPass objects for bones here.
 	//        Otherwise do whatever you like.
+
+
+	//Start Billboard code//
 	static const float bill_vertex_data[] = {
  	-5.0f, 0.0f, 1.0f,
  	5.0f, 0.0f, 1.0f,
@@ -282,6 +290,8 @@ int main(int argc, char* argv[])
 			{ std_model, std_view, std_proj},
 			{ "color" }
 			);
+
+	//End Billboard code//
 
 
 
